4167c96a62f7e78e32b4825bd9ea513252d3a7c7
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2025-09-29 16:28:01 +0530

    add gitignore and bump version -> 0.2.10

--- src/syft_serve/__init__.py
+++ src/syft_serve/__init__.py
@@ -31,7 +31,7 @@ Example:
 # Import only what we need for the public API
 from ._api import servers, create, terminate_all, ServerAlreadyExistsError, ServerNotFoundError
 
-__version__ = "0.2.9"
+__version__ = "0.2.10"
 
 # Clean up orphaned processes on module import
 import psutil
